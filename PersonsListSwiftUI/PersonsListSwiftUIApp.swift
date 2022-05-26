7c415bb017201847d3e2fbf37ab00f061e62e81c
Aleksandra Moiseenko <lesovskaya.aleks@mail.ru>
2022-05-26 14:31:04 +0300

    Initial Commit

---
+++ PersonsListSwiftUI/PersonsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonsListSwiftUIApp.swift
+//  PersonsListSwiftUI
+//
+//  Created by Александра Лесовская on 26.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
